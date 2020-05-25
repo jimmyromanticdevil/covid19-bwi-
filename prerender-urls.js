77cf6995c8ab4b80e4fa9f3fb34a2f11c82ae449
jimmy romanticdevil <romanticdevil.jimmy@gmail.com>
2020-05-25 22:10:12 +0700

    feat: add meta tag

--- prerender-urls.js
+++ prerender-urls.js
@@ -6,6 +6,7 @@ module.exports = async function () {
   const resp = await response.json();
   pages.push({
     url: "/",
+    title: "Data terkini COVID-19 di Banyuwangi versi cepat & hemat kuota.",
     data: resp.data,
   });
   return pages;
