81d3389de2dce253f1137073a5ebdc78b7b1f62b
jimmy romanticdevil <romanticdevil.jimmy@gmail.com>
2020-06-04 20:06:08 +0700

    feat: add differ data from previous

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,13 +1,86 @@
-module.exports = async function () {
-  const pages = []
-  const response = await fetch(
-    "https://corona.banyuwangikab.go.id/api/covid19"
+"use strict";
+
+const headers = {
+  "cache-control": "no-cache",
+  "x-apikey": "5ed5a3cc2032862ff2ce27d9",
+  "content-type": "application/json",
+};
+const domain = "https://covid19bwi-2d80.restdb.io";
+const coronaBwiUrl = "https://corona.banyuwangikab.go.id/api/covid19";
+
+async function writeData(responses) {
+  const url = `${domain}/rest/case`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({
+      covid_cases: responses,
+      last_updated: responses.last_updated,
+    }),
+    json: true,
+  });
+  let resp = await response.json();
+  return true || {};
+}
+
+async function updateData(responses, response_rest) {
+  const url = `${domain}/rest/case/${response_rest._id}`;
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: headers,
+    body: JSON.stringify({ covid_cases: responses }),
+  });
+  let resp = await response.json();
+  console.log(resp);
+  return true || {};
+}
+
+async function readData(responses) {
+  const fetchAllCases = await fetch(`${domain}/rest/case`, {
+    method: "GET",
+    headers: headers,
+  });
+
+  let getAllCases = await fetchAllCases.json();
+  const yesterday = ((d) => new Date(d.setDate(d.getDate() - 2)))(new Date())
+    .toISOString()
+    .slice(0, 10);
+
+  let yesterdayCase = getAllCases.find(
+    (el) => el.covid_cases.last_updated === yesterday
   );
-  const resp = await response.json();
+  let todayCase = getAllCases.find(
+    (el) => el.covid_cases.last_updated === responses.last_updated
+  );
+  if (todayCase) {
+    console.log("update data");
+    // first we need to update the data because gov API doesn't work like normal API
+    updateData(responses, todayCase);
+  } else {
+    console.log("insert data");
+    writeData(responses);
+  }
+
+  var myObj = {};
+  for (let [key, value] of Object.entries(responses)) {
+    if (key != "last_updated") {
+      myObj[`diff_${key}`] = value - yesterdayCase.covid_cases[key];
+    }
+  }
+  return myObj
+}
+
+module.exports = async function () {
+  //sorry guys this is hardcode, because I got deadline :p will fix it later.
+  const pages = [];
+  const response = await fetch(coronaBwiUrl);
+  let resp = await response.json();
+  let result = await readData(resp.data);
+  const data = {...result, ...resp.data}
   pages.push({
     url: "/",
     title: "Data terkini COVID-19 di Banyuwangi versi cepat & hemat kuota.",
-    data: resp.data,
+    data: data,
   });
   return pages;
 };
--- src/components/content-style/index.js
+++ src/components/content-style/index.js
@@ -1,65 +1,112 @@
-import { h } from 'preact';
+import { h, Component } from "preact";
 import style from "./style";
 import LayoutGrid from "preact-material-components/LayoutGrid";
 import "preact-material-components/LayoutGrid/style.css";
+import Snackbar from "preact-material-components/Snackbar";
+import "preact-material-components/Snackbar/style.css";
 
-const ContentStyle = (props) => {
-  const {covid_pengawasan, covid_rawat, ...items} = props.items;
-  const total_pengawasan = +covid_pengawasan + +covid_rawat
-  return (
-    <LayoutGrid>
-      <LayoutGrid.Inner>
-        <LayoutGrid.Cell cols="4">
-          <article>
-            <a>
-              <h3>
-                {items.total_odp} ODP <span>(Orang Dalam Pemantauan)</span>
-              </h3>
-              <section>
-                <div>
-                  <strong class={style.sembuh}>Sembuh <b>{items.odp_selesai}</b></strong>
-                  <strong class={style.pantau}>Pemantauan <b>{items.odp_proses}</b></strong>
-                  <strong class={style.meninggal}>Meninggal <b>{items.covid_meninggal}</b></strong>
-                </div>
-              </section>
-            </a>
-          </article>
-        </LayoutGrid.Cell>
-        <LayoutGrid.Cell cols="4">
-          <article>
-            <a>
-              <h3>
-                {items.total_pdp} PDP <span>(Pasien Dalam Pengawasan)</span>
-              </h3>
-              <section>
-                <div>
-                  <strong class={style.sembuh}>Sembuh <b>{items.pdp_sembuh}</b></strong>
-                  <strong class={style.pengawasan}>Pengawasan <b>{items.pdp_rawat}</b></strong>
-                  <strong class={style.meninggal}>Meninggal <b>{items.pdp_meninggal}</b></strong>
-                </div>
-              </section>
-            </a>
-          </article>
-        </LayoutGrid.Cell>
-        <LayoutGrid.Cell cols="4">
-          <article>
-            <a>
-              <h3>
-                {items.total_covid} COVID <span>(Positif Covid)</span>
-              </h3>
-              <section>
-                <div>
-                  <strong class={style.sembuh}>Sembuh <b>{items.covid_sembuh}</b></strong>
-                  <strong class={style.pengawasan}>Pengawasan <b>{total_pengawasan}</b></strong>
-                  <strong class={style.meninggal}>Meninggal <b>{items.covid_meninggal}</b></strong>
-                </div>
-              </section>
-            </a>
-          </article>
-        </LayoutGrid.Cell>
-      </LayoutGrid.Inner>
-    </LayoutGrid>
-  );
-};
+class ContentStyle extends Component {
+  render() {
+    const items = this.props.items;
+    return (
+      <LayoutGrid>
+        <LayoutGrid.Inner>
+          <LayoutGrid.Cell cols="4">
+            <article>
+              <a
+                raised={true}
+                onClick={() => {
+                  this.bar.MDComponent.show({
+                    message: `ODP ${items.total_odp} (+${items.diff_total_odp}) = S ${items.odp_selesai}(+${items.diff_odp_selesai}), P ${items.odp_proses}(+${items.diff_odp_proses}), M ${items.covid_meninggal}(+${items.diff_covid_meninggal})`,
+                  });
+                }}
+              >
+                <h3>
+                  {items.total_odp} ODP <span>(Orang Dalam Pemantauan)</span>
+                </h3>
+                <section>
+                  <div>
+                    <strong class={style.sembuh}>
+                      Sembuh <b>{items.odp_selesai}</b>
+                    </strong>
+                    <strong class={style.pantau}>
+                      Pemantauan <b>{items.odp_proses}</b>
+                    </strong>
+                    <strong class={style.meninggal}>
+                      Meninggal <b>{items.covid_meninggal}</b>
+                    </strong>
+                  </div>
+                </section>
+              </a>
+            </article>
+          </LayoutGrid.Cell>
+          <LayoutGrid.Cell cols="4">
+            <article>
+              <a
+                raised={true}
+                onClick={() => {
+                  this.bar.MDComponent.show({
+                    message: `PDP ${items.total_pdp} (+${items.diff_total_pdp}) = S ${items.pdp_sembuh}(+${items.diff_pdp_sembuh}), P ${items.pdp_rawat}(+${items.diff_pdp_rawat}), M ${items.pdp_meninggal}(+${items.pdp_meninggal})`,
+                  });
+                }}
+              >
+                <h3>
+                  {items.total_pdp} PDP <span>(Pasien Dalam Pengawasan)</span>
+                </h3>
+                <section>
+                  <div>
+                    <strong class={style.sembuh}>
+                      Sembuh <b>{items.pdp_sembuh}</b>
+                    </strong>
+                    <strong class={style.pengawasan}>
+                      Pengawasan <b>{items.pdp_rawat}</b>
+                    </strong>
+                    <strong class={style.meninggal}>
+                      Meninggal <b>{items.pdp_meninggal}</b>
+                    </strong>
+                  </div>
+                </section>
+              </a>
+            </article>
+          </LayoutGrid.Cell>
+          <LayoutGrid.Cell cols="4">
+            <article>
+              <a
+                raised={true}
+                onClick={() => {
+                  this.bar.MDComponent.show({
+                    message: `COVID ${items.total_covid} (+${items.diff_total_covid}) = S ${items.covid_sembuh}(+${items.diff_covid_sembuh}), P ${items.covid_pengawasan}(+${items.diff_covid_pengawasan}), M ${items.covid_meninggal}(+${items.covid_meninggal})`,
+                  });
+                }}
+              >
+                <h3>
+                  {items.total_covid} COVID <span>(Positif Covid)</span>
+                </h3>
+                <section>
+                  <div>
+                    <strong class={style.sembuh}>
+                      Sembuh <b>{items.covid_sembuh}</b>
+                    </strong>
+                    <strong class={style.pengawasan}>
+                      Pengawasan <b>{items.covid_pengawasan}</b>
+                    </strong>
+                    <strong class={style.meninggal}>
+                      Meninggal <b>{items.covid_meninggal}</b>
+                    </strong>
+                  </div>
+                </section>
+              </a>
+            </article>
+          </LayoutGrid.Cell>
+        </LayoutGrid.Inner>
+        <Snackbar
+          ref={(bar) => {
+            this.bar = bar;
+          }}
+        />
+      </LayoutGrid>
+    );
+  }
+}
 
 export default ContentStyle;
